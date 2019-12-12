08a56768c045a881f52f1d8cf526684f40f37378
twtseng <twtseng@indeed.com>
2019-12-12 13:16:51 +0900

    Initial Commit

---
+++ SimpleWebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleWebView
+//
+//  Created by Tai Wei Tseng on 2019/12/12.
+//  Copyright © 2019 man27382210. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
